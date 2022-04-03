2236566ad0463a1e1d266de6ee9db23d00e258ad
Tobias Schröpfer <tobias-schroepfer@web.de>
2022-04-03 21:46:15 +0200

    Search field component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFieldComponent } from './components/filter-bar/controls/search-field/search-field.component';
 import { FilterBarComponent } from './components/filter-bar/filter-bar.component';
 import { FilterableTableComponent } from './components/filterable-table/filterable-table.component';
 import { SecuritiesListComponent } from './components/securities-list/securities-list.component';
@@ -15,11 +18,14 @@ import { SecuritiesListComponent } from './components/securities-list/securities
     FilterableTableComponent,
     SecuritiesListComponent,
     FilterBarComponent,
+    SearchFieldComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
+    MatIconModule,
+    MatInputModule,
     MatTableModule,
     MatProgressSpinnerModule,
   ],
