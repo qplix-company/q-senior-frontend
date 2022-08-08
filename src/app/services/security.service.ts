5b214d7506d12d71a52d946411f1da9807d82620
Thilo Kochanowski <git@thilo-kochanowski.de>
2022-08-08 11:38:35 +0200

    setup filterDefs for securities

--- src/app/services/security.service.ts
+++ src/app/services/security.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { delay, lastValueFrom, Observable, of } from 'rxjs';
 import { Security } from '../models/security';
 import { SECURITIES } from '../mocks/securities-mock';
 import { SecuritiesFilter } from '../models/securitiesFilter';
+import { FilterDefinition, FilterType } from '../models/filter';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SecurityService {
-  constructor() {}
-
   /**
    * Get Securities server request mock
    * */
@@ -20,6 +19,49 @@ export class SecurityService {
     );
 
     return of(filteredSecurities).pipe(delay(1000));
+  }
+
+  async getFilterdefinitions(): Promise<FilterDefinition[]> {
+    const securityTypes = await this.getSecurityTypes();
+    const securityCurrencies = await this.getSecurityCurrencies();
+    return [
+      {
+        label: 'Name',
+        type: FilterType.input,
+        filterKey: 'name',
+      },
+      {
+        label: 'Type',
+        type: FilterType.selectMultiple,
+        filterKey: 'types',
+        options: securityTypes,
+      },
+      {
+        label: 'Currency',
+        type: FilterType.selectMultiple,
+        filterKey: 'currencies',
+        options: securityCurrencies,
+      },
+      {
+        label: 'is Private',
+        type: FilterType.checkbox,
+        filterKey: 'isPrivate',
+      },
+    ];
+  }
+
+  private getSecurityTypes(): Promise<string[]> {
+    const securityTypes: Set<string> = new Set();
+
+    SECURITIES.map((security: Security) => securityTypes.add(security.type));
+    return lastValueFrom(of(Array.from(securityTypes).sort()).pipe(delay(1000)));
+  }
+
+  private getSecurityCurrencies(): Promise<string[]> {
+    const securityCurrencies: Set<string> = new Set();
+
+    SECURITIES.map((security: Security) => securityCurrencies.add(security.currency));
+    return lastValueFrom(of(Array.from(securityCurrencies).sort()).pipe(delay(1000)));
   }
 
   private filterSecurities(securityFilter: SecuritiesFilter) {
