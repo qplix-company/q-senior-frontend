b9b74b29e2058d86fa257bd43dde0336665b9c87
Alex Raudzus <alexander.raudzus@qplix.com>
2025-09-07 22:27:40 +0200

    Baseline for task 1 and 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { SecuritiesListComponent } from './components/securities-list/securities-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Task1Component } from './tasks/task1/task1.component';
+import { Task2Component } from './tasks/task2/task2.component';
+import { Task3Component } from './tasks/task3/task3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SecuritiesListComponent],
+  imports: [
+    MatExpansionModule,
+    MatTabsModule,
+    Task1Component,
+    Task2Component,
+    Task3Component,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/tasks/task2/task2.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-task2',
+  imports: [],
+  standalone: true,
+  templateUrl: './task2.component.html',
+  styleUrls: ['./task2.component.scss'],
+})
+export class Task2Component {
+
+}
